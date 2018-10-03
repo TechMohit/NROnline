2ff0c2e91ce66300dba0eabc75af647534a1fd38
subhamnaik15 <subhamnaik15@gmail.com>
2018-10-04 00:19:28 +0530

    login ui start

--- app/src/main/java/grossary/cyron/com/grossary/account/SigninActivity.java
+++ app/src/main/java/grossary/cyron/com/grossary/account/SigninActivity.java
@@ -3,17 +3,20 @@ package grossary.cyron.com.grossary.account;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import grossary.cyron.com.grossary.home.HomeActivity;
 import grossary.cyron.com.grossary.R;
 
 public class SigninActivity extends AppCompatActivity {
 
+
+    private Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        startActivity(new Intent(SigninActivity.this, HomeActivity.class));
+//        startActivity(new Intent(SigninActivity.this, HomeActivity.class));
 
     }
 }
